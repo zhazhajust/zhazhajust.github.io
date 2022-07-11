ba08a132f47edc0cc965471059246618ef35def0
zhazhajust <251338258@qq.com>
2022-07-11 22:43:41 +0800

    new

--- pages/posts/[pid].js
+++ pages/posts/[pid].js
@@ -1,10 +1,22 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+//import { getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import "highlight.js/styles/github.css";
+import hljs from "highlight.js/lib/core";
+//import javascript from "highlight.js/lib/languages/javascript";
+//import cpp from "highlight.js/lib/languages/cpp";
+//import python from "highlight.js/lib/languages/python";
+import { useEffect } from "react";
 
 export default function Post({ postData }) {
+  useEffect(() => {
+    hljs.registerLanguage('cpp',          require('highlight.js/lib/languages/cpp'));
+    hljs.registerLanguage('python',       require('highlight.js/lib/languages/python'));
+    hljs.highlightAll();
+  });
   return (
     <Layout>
       <Head>
@@ -22,7 +34,8 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  var paths = getAllPostIds();
+  console.log(paths)
   return {
     paths,
     fallback: false
@@ -30,7 +43,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  //const postData = await getPostData(params.id)
+  //const router = useRouter()
+  //const { id } = router.query
+  console.log(`Params: ${params}`)
+  const postData = await getPostData(params.pid)
   return {
     props: {
       postData
